27d910bac4bd87103e3aee32e8ef3e65b42031a4
Brennan Reinhard <brennanreinhard15@gmail.com>
2022-07-27 21:16:04 -0500

    Big changes
    Lots of exceptions, settings, and more!

--- homescreen.py
+++ homescreen.py
@@ -1,24 +1,23 @@
-# shoe
+# sho
 # Function File Imports
-from functions import *
+from time import sleep
 # Module Imports
-import sys
-import os
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Style
 from configparser import ConfigParser
-import glob
-
+import os
+from cryptography.fernet import Fernet
 init()  # initialize colorama
-file = 'config.ini'
-config = ConfigParser()
-config.read(file)
+clear = lambda: os.system('cls')  # define clear
+
+configsetupfile = 'config.ini'  # conf file
+parser = ConfigParser()  # define configparser object
+parser.read(configsetupfile)  # read file
+pathofentries = parser.get('settings', 'pathofentries')
+pathofkeys = parser.get('settings', 'pathofkeys')
 
 
 def home():
-    # main def const
-    menu_actions = {}
-
-    # small menu functions
+    # small menu functions (usually just printing stuff out to program)
     def menu_what_is_dayli():
         print("Dayli is a private, secure way to write down daily diary entires on your computer.")
         input("Press Enter to continue...")
@@ -51,21 +50,21 @@ def home():
         print("5. Settings")
         print("\n0. Quit")
         choice = input(" >>  ")
-        exec_menu(choice)
-
-        return
-
-    def exec_menu(choice):
-        clear()
-        ch = choice.lower()
-        if ch == '':
-            menu_actions['main_menu']()
-        else:
-            try:
-                menu_actions[ch]()
-            except KeyError:
-                print("StOp tRyInG tO bReAk My PrOgRaM!!1! D:\n")
-                menu_actions['main_menu']()
+        if choice == "1":
+            clear()
+            menu1()
+        elif choice == "2":
+            clear()
+            menu2()
+        elif choice == "3":
+            clear()
+            menu3()
+        elif choice == "4":
+            clear()
+            menu4()
+        elif choice == "5":
+            clear()
+            menu5()
         return
 
     # Menu 1
@@ -77,50 +76,112 @@ def home():
 
     # Menu 2
     def menu2():
-        print(" ")
-        time.sleep(3)
+        print(" Uhhhh, sorry. This file is MISSING (OH NOES!)")
+        sleep(3)
         main_menu()
 
     def menu3():
-        print("Sorry, still in development! Release TBD")
-        time.sleep(3)
-        main_menu()
-        choice = input(" >>  ")
-        exec_menu(choice)
+        unencrypt()
         return
 
     def menu4():
         menu_what_is_dayli()
+        return
 
     def menu5():
+        clear()
+        settings()
+        return
 
+    # BIG FUNCTIONS
+    def entry():
+        filename = input(
+            "What would you like to call this entry? No special characters, like /, \, ', etc. (Subject, "
+            "date (Using periods), etc): "
+        )
+        filename += ".txt"
+        makeentry = os.path.join(
+            pathofentries, filename
+        )
 
-    # Back to main menu
-    def back():
-        menu_actions['main_menu']()
+        writeentry = open(makeentry, "w")
 
-    # Exit program
-    def exit_menu():
-        sys.exit()
+        clear()
+        entry.entrybodyglob = input(
+            "Welcome to the entry maker! Write what you want, then hit enter when you are finished: "
+        )
 
-    # =======================
-    #    MENUS DEFINITIONS
-    # =======================
+        clear()
 
-    # Menu definition
-    menu_actions = {
-        'main_menu': main_menu,
-        '1': menu1,
-        '2': menu2,
-        '3': menu3,
-        '4': menu4,
-        '5': menu5,
-        '9': back,
-        '0': exit_menu
-    }
+        writeentry.write(str(entry.entrybodyglob))
+        encryptquestion = input("Would you like to encrypt this file? ")
+        encryptquestion.lower()
 
-    # =======================
-    #      MAIN PROGRAM
-    # =======================
+        # Encryption stuff
+        if encryptquestion == "yes":
+            key = Fernet.generate_key()
+            encryptpath = os.path.join(
+                pathofkeys, filename
+            )
+            encryptpath += ".key"
+
+            with open(encryptpath, "wb") as filekey:
+                filekey.write(key)
+
+            # opening the key
+            with open(encryptpath, "rb") as filekey:
+                key = filekey.read()
+
+            # using the generated key
+            fernet = Fernet(key)
+
+            # opening the original file to encrypt
+            with open(makeentry, "rb") as file:
+                original = file.read()
+
+            # encrypting the file
+            encrypted = fernet.encrypt(original)
+
+            # opening the file in write mode and
+            # writing the encrypted data
+            with open(makeentry, "wb") as encrypted_file:
+                encrypted_file.write(encrypted)
+            print("Encrypted! Also, entry added!")
+        elif encryptquestion == "no":
+            print("Ok, not encrypting. Entry added!")
+        else:
+            print("Please enter a valid option...")
+
+        sleep(2.5)
+        clear()
+        return
+
+    def unencrypt():
+        print(
+            "Listing all files in entry directory (TIP: If there is a lot of files, go into file explorer and type in your file from there.")
+        for listfiles in os.listdir(pathofentries):
+            if listfiles.endswith(".txt"):
+                # Prints only text file present in My Folder
+                print(listfiles)
+        input("Press enter")
+        home()
+
+    def settings():
+        settingselector = input("Welcome to settings! Please input an option number for the thing you want to change.")
+        print("1. Change path of entries")
+        print("2. Change path of encryption keys")
+        print("3. Change name or add name")
+        if settingselector == "1":
+            print("Do something")
+        elif settingselector == "2":
+            print("Do something")
+        elif settingselector == "3":
+            print("Do something")
+        elif settingselector == "4":
+            print("Do something")
+        else:
+            print("Please enter again.")
+            settings()
+
 
     main_menu()
--- main.py
+++ main.py
@@ -1,8 +1,93 @@
 # shoe 
 from time import sleep
-import os
-from functions import *
+from homescreen import *
 from homescreen import home
+import configparser
+import sys
+import os
+# grab vars from ini
+
+pathofentries = parser.get('settings', 'pathofentries')
+pathofkeys = parser.get('settings', 'pathofkeys')
+username = parser.get('settings', 'name')
+firsttime = parser.get('mostlystaticvars', 'firsttime')
+clear()
+
+entriesdirectoryexists = os.path.isdir(pathofentries)
+if entriesdirectoryexists:
+    print("Entries directory found")
+else:
+    pathofentriesinput = input(
+        "Entries directory not found. It may have been relocated, renamed, or deleted. It also may have been entered incorrectly. Would you like to (1) change the path to the entries directory, or (2) quit the program? ")
+    if pathofentriesinput == '1':
+        clear()
+        while not entriesdirectoryexists:
+            print(r"Please enter the new path to the entries directory. Add it like this: C:\Users\Owner\Desktop\Entries: ")
+            newEntriesPath = input()
+
+            parser.set('settings', 'pathofentries', newEntriesPath)
+            entriesdirectoryexists = os.path.isdir(newEntriesPath)
+            with open(configsetupfile, "w") as conf_file:
+                parser.write(conf_file)
+            entriesdirectoryexists = entriesdirectoryexists
+            clear()
+    if pathofentriesinput == '2':
+        sys.exit()
+
+if firsttime == 'yes':
+    print("Hello, and welcome to Dayli!")
+    input("Press Enter to continue...")
+    clear()
+    print("Dayli is a secure way to make virtual diary entries on the computer.")
+    input("Press Enter to continue...")
+    clear()
+    print("You have the option to encrypt files, read files, and more!")
+    input("Press Enter to continue...")
+    clear()
+    print(
+        r"First we have to do some setup. Where would you like to have your keys for your encrypted files stored? (Add it as a full path like this: C:\Users\Owner\Desktop\EncryptKeys): ")
+    firsttimekeypath = input()
+
+    parser.set('settings', 'pathofkeys', firsttimekeypath)
+    with open(configsetupfile, "w") as conf_file:
+        parser.write(conf_file)
+
+    print(
+        r"Now let's set where you want your entries stored. (Add it as a full path like this: C:\Users\Owner\Desktop\Entries")
+    firsttimeentrypath = input()
+
+    parser.set('settings', 'pathofentries', firsttimeentrypath)
+    with open(configsetupfile, "w") as conf_file:
+        parser.write(conf_file)
+
+    print(
+        "Last but not least, would you like to add a username? This is completely optional, and is not required. Type in 1 if you do want to do a username, and type in 2 if you don't. ")
+    nameofusermenu = input()
+    if nameofusermenu == '1':
+        print("What do you want your username to be? ")
+        nameofuser = input()
+
+        parser.set('settings', 'name', nameofuser)
+        with open(configsetupfile, "w") as conf_file:
+            parser.write(conf_file)
+
+    elif nameofusermenu == '2':
+        parser.set('mostlystaticvars', 'usesname', 'no')
+        with open(configsetupfile, "w") as conf_file:
+            parser.write(conf_file)
+
+        print(
+            "Ok, that's fine. And remember, you can change any of these settings in the settings menu of the home screen. Thanks for using Dayli!")
+        sleep(2.5)
+
+    else:
+        print("Please select a valid option.")
+
+    parser.set('mostlystaticvars', 'firsttime', 'no')
+    with open(configsetupfile, "w") as conf_file:
+        parser.write(conf_file)
+
+
 clear()
 print("\n\n\n")
 print("      $$\                     $$\ $$\ ")
@@ -17,8 +102,6 @@ print("                    $$\   $$ |        ")
 print("                    \$$$$$$  |        ")
 print("                     \______/         ")
 
-
 sleep(3)
 clear()
 home()
-                                                                                                     
